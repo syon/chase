0564d56bf9cc2ab1d24926adcfa371b8032d0bbf
syon <syon.xiii@gmail.com>
2016-05-15 13:12:25 +0900

    svg

--- after/main.sh
+++ after/main.sh
@@ -9,6 +9,7 @@ bundle exec ruby after/items_ogp.rb
 # convert to jpg
 mogrify -format jpg after/thumbs/*/*.gif
 mogrify -format jpg after/thumbs/*/*.png
+mogrify -format jpg after/thumbs/*/*.svg
 
 # resize width 200px
 mogrify -resize 200x after/thumbs/*/*.jpg
@@ -16,6 +17,7 @@ mogrify -resize 200x after/thumbs/*/*.jpg
 # delete except jpg
 find after/thumbs -type f -name '*.gif' -delete
 find after/thumbs -type f -name '*.png' -delete
+find after/thumbs -type f -name '*.svg' -delete
 find after/thumbs -type f -name '*.jpg~' -delete
 
 # sync with S3
