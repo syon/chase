291b2c8d243ab7d92099c62769cb280fcf8f33ae
syon <syon.xiii@gmail.com>
2016-05-17 20:29:09 +0900

    fill white on converting png

--- after/main.sh
+++ after/main.sh
@@ -14,7 +14,7 @@ echo "\033[1;46m Convert and resize \033[0;39m"
 
 # convert to jpg
 mogrify -format jpg after/thumbs/*/*.gif
-mogrify -format jpg after/thumbs/*/*.png
+mogrify -format jpg -fill "#FFFFFF" -opaque none after/thumbs/*/*.png
 mogrify -format jpg after/thumbs/*/*.svg
 
 # resize width 200px
