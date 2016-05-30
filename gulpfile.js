07014faeb9531a5a92a44bae807826fb56ed704b
syon <syon.xiii@gmail.com>
2016-05-30 21:38:04 +0900

    Less

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,39 @@ var gulp = require('gulp'),
     babelify = require('babelify'),
     browserify = require('browserify'),
     minifyify = require('minifyify'),
-    watchify = require('watchify');
+    watchify = require('watchify'),
+    less = require('gulp-less'),
+    path = require('path');
 
 gulp.task('browserify', function () {
-    var bundler = browserify({
-        entries: ['babel/app.js'],
-        transform: [[babelify, {presets: ["es2015", "react"]}]],
-        debug: true // sourcemapping
-    });
+  var bundler = browserify({
+      entries: ['babel/app.js'],
+      transform: [[babelify, {presets: ["es2015", "react"]}]],
+      debug: true // sourcemapping
+  });
 
-    bundler.plugin('minifyify', {map: 'map.json', output: './public/js/map.json'});
+  bundler.plugin('minifyify', {map: 'map.json', output: './public/js/map.json'});
 
-    var watcher = watchify(bundler);
-    return watcher
-      .on('update', function () {
-        var updateStart = Date.now();
-        watcher.bundle()
-          .pipe(source('bundle.js'))
-          .pipe(gulp.dest('./public/js/'));
-        console.log('Updated in ', (Date.now() - updateStart) + 'ms');
-      })
-      .bundle()
-      .pipe(source('bundle.js'))
-      .pipe(gulp.dest('./public/js/'));
+  var watcher = watchify(bundler);
+  return watcher
+    .on('update', function () {
+      var updateStart = Date.now();
+      watcher.bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./public/js/'));
+      console.log('Updated in ', (Date.now() - updateStart) + 'ms');
+    })
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./public/js/'));
 });
 
-gulp.task('default', ['browserify']);
+gulp.task('less', function () {
+  return gulp.src('./less/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('default', ['browserify', 'less']);
