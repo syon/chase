ca5272d45d16646cdc175778a26d1e87fb9df6d1
syon <syon.xiii@gmail.com>
2016-06-21 22:31:59 +0900

    :art: TopBar is stateless component

--- babel/my/TopBar.js
+++ babel/my/TopBar.js
@@ -7,43 +7,33 @@ import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import SyncDisabled from 'material-ui/svg-icons/notification/sync-disabled';
 import OpenInNew from 'material-ui/svg-icons/action/open-in-new';
 
-class TopBar extends React.Component {
-  openPocket() {
-    window.open('https://getpocket.com');
-  }
-
-  exitApp() {
-    window.location = '/disconnect';
-  }
-
-  render() {
-    return (
-      <AppBar
-        title="Chase"
-        showMenuIconButton={false}
-        iconElementRight={
-          <IconMenu
-            iconButtonElement={
-              <IconButton><MoreVertIcon /></IconButton>
-            }
-            targetOrigin={{ horizontal: 'right', vertical: 'top' }}
-            anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
-          >
-            <MenuItem
-              primaryText="Pocket"
-              leftIcon={<OpenInNew />}
-              onTouchTap={this.openPocket}
-            />
-            <MenuItem
-              primaryText="Disconnect"
-              leftIcon={<SyncDisabled />}
-              onTouchTap={this.exitApp}
-            />
-          </IconMenu>
-        }
-      />
-    );
-  }
+function TopBar() {
+  return (
+    <AppBar
+      title="Chase"
+      showMenuIconButton={false}
+      iconElementRight={
+        <IconMenu
+          iconButtonElement={
+            <IconButton><MoreVertIcon /></IconButton>
+          }
+          targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+          anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+        >
+          <MenuItem
+            primaryText="Pocket"
+            leftIcon={<OpenInNew />}
+            onTouchTap={() => window.open('https://getpocket.com')}
+          />
+          <MenuItem
+            primaryText="Disconnect"
+            leftIcon={<SyncDisabled />}
+            onTouchTap={() => (window.location = '/disconnect')}
+          />
+        </IconMenu>
+      }
+    />
+  );
 }
 
 export default TopBar;
