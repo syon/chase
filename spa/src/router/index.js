abd615077d6b6ff58ba7836afece2e8fd7ed4c72
syon <syon.xiii@gmail.com>
2017-11-19 21:44:38 +0900

    :lipstick: app.stylus

--- spa/src/router/index.js
+++ spa/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
 
+import '@/assets/app.stylus';
+
 Vue.use(Router);
 
 export default new Router({
