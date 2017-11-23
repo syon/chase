2227b6c936e21d1d15b630d4cda0303a19896391
syon <syon.xiii@gmail.com>
2017-11-23 22:24:27 +0900

    :file_folder: lib/PocketAPI

--- ServerlessAPI/handler.js
+++ ServerlessAPI/handler.js
@@ -11,128 +11,24 @@
 // http://docs.aws.amazon.com/ja_jp/apigateway/latest/developerguide/how-to-cors.html
 // https://qiita.com/maaz118/items/e20b64f088fbead07206
 
-require('dotenv').config();
 const AWS = require('aws-sdk');
 const gm = require('gm').subClass({ imageMagick: true });
 const axios = require('axios');
 
+const PocketAPI = require('./lib/PocketAPI');
 const Libra = require('./lib/Libra');
 const s3 = new AWS.S3();
 
-const redirectUri = 'https://syon-chase.herokuapp.com?authorizationFinished';
-const HTTP_CONFIG = {
-  headers: {
-    'Content-Type': 'application/json; charset=UTF-8',
-    'X-Accept': 'application/json',
-  },
-};
-
-function successResponseBuilder(bodyObj) {
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin" : "*",
-    },
-    body: JSON.stringify(bodyObj),
-  };
-}
-
-function errorResponseBuilder(error) {
-  console.log(error);
-  const res = error.response || { headers: {} };
-  const response = {
-    statusCode: res.status,
-    headers: {
-      "Access-Control-Allow-Origin" : "*",
-      'x-error-code': res.headers['x-error-code'],
-      'x-error': res.headers['x-error'],
-    },
-    body: JSON.stringify({
-      message: res.statusText,
-    }),
-  };
-  return response;
-}
-
 module.exports.pocketOauthRequest = (event, context, callback) => {
-  const reqd = {
-    'consumer_key': process.env.POCKET_CONSUMER_KEY,
-    'redirect_uri': 'https://syon-chase.herokuapp.com?redirected',
-  };
-  const data = JSON.stringify(reqd);
-  axios.post('https://getpocket.com/v3/oauth/request', data, HTTP_CONFIG)
-    .then(function (res) {
-      const requestToken = res.data.code;
-      const authUri = `https://getpocket.com/auth/authorize?request_token=${requestToken}&redirect_uri=${redirectUri}`
-      const response = {
-        statusCode: 200,
-        headers: {
-          "Access-Control-Allow-Origin" : "*",
-        },
-        body: JSON.stringify({
-          request_token: requestToken,
-          auth_uri: authUri,
-        }),
-      };
-      callback(null, response);
-    })
-    .catch(function (error) {
-      console.log(error);
-      callback(null, errorResponseBuilder(error));
-    });
+  return PocketAPI.pocketOauthRequest(event, context, callback);
 };
 
 module.exports.pocketOauthAuthorize = (event, context, callback) => {
-  const params = JSON.parse(event.body);
-  const reqd = {
-    'consumer_key': process.env.POCKET_CONSUMER_KEY,
-    'code': params.code,
-  };
-  const data = JSON.stringify(reqd);
-  axios.post('https://getpocket.com/v3/oauth/authorize', data, HTTP_CONFIG)
-    .then(function (res) {
-      const at = res.data.access_token;
-      const un = res.data.username;
-      const response = {
-        statusCode: 200,
-        headers: {
-          "Access-Control-Allow-Origin" : "*",
-        },
-        body: JSON.stringify({
-          access_token: at,
-          username: un,
-        }),
-      };
-      callback(null, response);
-    })
-    .catch(function (error) {
-      console.log(error);
-      callback(null, errorResponseBuilder(error));
-    });
+  return PocketAPI.pocketOauthAuthorize(event, context, callback);
 };
 
 module.exports.pocketGet = (event, context, callback) => {
-  const params = JSON.parse(event.body);
-  const reqd = {
-    'consumer_key': process.env.POCKET_CONSUMER_KEY,
-    'access_token': params.access_token,
-  };
-  const data = JSON.stringify(Object.assign(reqd, params));
-  axios.post('https://getpocket.com/v3/get', data, HTTP_CONFIG)
-    .then(function (res) {
-      const response = {
-        statusCode: 200,
-        headers: {
-          "Access-Control-Allow-Origin" : "*",
-        },
-        body: JSON.stringify(res.data),
-      };
-      callback(null, response);
-    })
-    .catch(function (error) {
-      console.log(error);
-      callback(null, errorResponseBuilder(error));
-    });
+  return PocketAPI.pocketGet(event, context, callback);
 };
 
 module.exports.libraInfo = (event, context, callback) => {
