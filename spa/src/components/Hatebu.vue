0d9c9102a98773dabe6fe7095b6a1a3480820175
syon <syon.xiii@gmail.com>
2017-12-10 18:11:24 +0900

    :lipstick: Hatebu styling

--- spa/src/components/Hatebu.vue
+++ spa/src/components/Hatebu.vue
@@ -5,7 +5,12 @@
     h4 {{ hatebu.count }}
   .bookmarks
     .voice(v-for="b in voices")
-      span {{ b.comment }}
+      .meta
+        .user
+          img.avatar(:src="avatarUrl(b)")
+          .username {{ b.user }}
+        .date {{ commentDate(b) }}
+      .comment {{ b.comment }}
 </template>
 
 <script>
@@ -29,6 +34,15 @@ export default {
       return this.hatebu.bookmarks.filter(d => d.comment.length !== 0);
     },
   },
+  methods: {
+    avatarUrl(b) {
+      return `//cdn1.www.st-hatena.com/users/${b.user}/profile_l.gif`;
+    },
+    commentDate(b) {
+      const ymd = b.timestamp.match(/^(20..\/..\/..)/)[1];
+      return ymd.replace(/\//g, '.');
+    },
+  },
 };
 </script>
 
@@ -48,7 +62,26 @@ export default {
 .bookmarks
   flex 1
   overflow auto
+  padding-bottom 100px
   .voice
-    padding 0 0 .5em
+    padding 0 0 .75em
     font-size .8rem
+    word-break break-all
+    .meta
+      display flex
+      align-items center
+      justify-content space-between
+      font-size 0.6rem
+      color #757575
+      .user
+        display flex
+        align-items center
+      .avatar
+        width 16px
+        height 16px
+        border-radius 1.5px
+      .username
+        padding 0 0.5em
+    .comment
+      margin .25em 0 .5em
 </style>
