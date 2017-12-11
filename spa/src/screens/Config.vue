3f730b65b6654e574f8d0f9184b936ae24cf3043
syon <syon.xiii@gmail.com>
2017-12-11 21:44:57 +0900

    :cyclone: Config page layout

--- spa/src/screens/Config.vue
+++ spa/src/screens/Config.vue
@@ -1,26 +1,33 @@
 <template lang="pug">
-.compo
-  h2 Config
-  section.scene
-    label
-      span chase:a
-      input(v-model="chaseA")
-    hr
-    label
-      span chase:b
-      input(v-model="chaseB")
-    hr
-    label
-      span chase:c
-      input(v-model="chaseC")
-    hr
-    button(@click="doSceneEdit") OK
+.screen.page-container
+  nav.page-nav
+    sidebar
+  article.page-content
+    h2 Config
+    section.scene
+      label
+        span chase:a
+        input(v-model="chaseA")
+      hr
+      label
+        span chase:b
+        input(v-model="chaseB")
+      hr
+      label
+        span chase:c
+        input(v-model="chaseC")
+      hr
+      button(@click="doSceneEdit") OK
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import Sidebar from '@/components/Sidebar';
 
 export default {
+  components: {
+    Sidebar,
+  },
   data() {
     return {
       sceneEditing: false,
@@ -47,7 +54,19 @@ export default {
       const scenes = { a: this.chaseA, b: this.chaseB, c: this.chaseC };
       this.$store.dispatch('doSceneEdit', { $cookie: this.$cookie, scenes });
       this.sceneEditing = false;
+      this.$router.push({ name: 'Inbox' });
     },
   },
 };
 </script>
+
+<style lang="stylus" scoped>
+.page-container
+  width 100%
+  display flex
+  .page-nav
+    width 180px
+  .page-content
+    flex 1
+    padding 0 15px
+</style>
