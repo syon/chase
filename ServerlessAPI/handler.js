9b4a627b5ad5dcb25261fd4bb3bb6365ae64e05c
syon <syon.xiii@gmail.com>
2017-12-29 12:29:01 +0900

    :construction: rename

--- ServerlessAPI/handler.js
+++ ServerlessAPI/handler.js
@@ -11,37 +11,37 @@
 // http://docs.aws.amazon.com/ja_jp/apigateway/latest/developerguide/how-to-cors.html
 // https://qiita.com/maaz118/items/e20b64f088fbead07206
 
-const PocketAPI = require('./lib/PocketAPI');
+const PocketAdaptor = require('./lib/PocketAdaptor');
 const UserAdaptor = require('./lib/UserAdaptor');
 const FilmAdaptor = require('./lib/FilmAdaptor');
 const LibraAdaptor = require('./lib/LibraAdaptor');
 
 module.exports.pocketOauthRequest = (event, context, callback) => {
-  return PocketAPI.pocketOauthRequest(event, context, callback);
+  return PocketAdaptor.pocketOauthRequest(event, context, callback);
 };
 
 module.exports.pocketOauthAuthorize = (event, context, callback) => {
-  return PocketAPI.pocketOauthAuthorize(event, context, callback);
+  return PocketAdaptor.pocketOauthAuthorize(event, context, callback);
 };
 
 module.exports.pocketGet = (event, context, callback) => {
-  return PocketAPI.pocketGet(event, context, callback);
+  return PocketAdaptor.pocketGet(event, context, callback);
 };
 
 module.exports.pocketSendArchive = (event, context, callback) => {
-  return PocketAPI.pocketSendArchive(event, context, callback);
+  return PocketAdaptor.pocketSendArchive(event, context, callback);
 };
 
 module.exports.pocketSendFavorite = (event, context, callback) => {
-  return PocketAPI.pocketSendFavorite(event, context, callback);
+  return PocketAdaptor.pocketSendFavorite(event, context, callback);
 };
 
 module.exports.pocketSendUnfavorite = (event, context, callback) => {
-  return PocketAPI.pocketSendUnfavorite(event, context, callback);
+  return PocketAdaptor.pocketSendUnfavorite(event, context, callback);
 };
 
 module.exports.pocketSendTagsAdd = (event, context, callback) => {
-  return PocketAPI.pocketSendTagsAdd(event, context, callback);
+  return PocketAdaptor.pocketSendTagsAdd(event, context, callback);
 };
 
 module.exports.userregister = (event, context, callback) => {
--- ServerlessAPI/lib/PocketAPI.js
+++ ServerlessAPI/lib/PocketAPI.js
@@ -1,15 +1,4 @@
-require('dotenv').config();
 const axios = require('axios');
-const URLSearchParams = require('url-search-params');
-const debug = require('debug')('chase:film');
-
-const Pocket = require('./PocketCore');
-
-const HTTP_GET_CONFIG = {
-  headers: {
-    'X-Accept': 'application/json',
-  },
-};
 
 const HTTP_POST_CONFIG = {
   headers: {
@@ -18,157 +7,13 @@ const HTTP_POST_CONFIG = {
   },
 };
 
-function successResponseBuilder(bodyObj) {
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-    body: JSON.stringify(bodyObj),
-  };
-}
-
-function errorResponseBuilder(error) {
-  const res = error.response || { headers: {} };
-  debug(res);
-  const response = {
-    statusCode: res.status,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'x-error-code': res.headers['x-error-code'],
-      'x-error': res.headers['x-error'],
-    },
-    body: JSON.stringify({
-      message: res.statusText,
-    }),
-  };
-  return response;
-}
-
-module.exports.pocketOauthRequest = (event, context, callback) => {
-  const params = JSON.parse(event.body);
+module.exports.get = (ck, at, params) => {
   const reqd = {
-    consumer_key: process.env.POCKET_CONSUMER_KEY,
-    redirect_uri: params.redirect_uri,
+    consumer_key: ck,
+    access_token: at,
   };
-  const data = JSON.stringify(reqd);
-  axios.post('https://getpocket.com/v3/oauth/request', data, HTTP_POST_CONFIG)
-    .then((res) => {
-      const requestToken = res.data.code;
-      const authUri = `https://getpocket.com/auth/authorize?request_token=${requestToken}&redirect_uri=${params.redirect_uri}`;
-      const bodyObj = {
-        request_token: requestToken,
-        auth_uri: authUri,
-      };
-      callback(null, successResponseBuilder(bodyObj));
-    })
-    .catch((error) => {
-      debug(error);
-      callback(null, errorResponseBuilder(error));
-    });
-};
-
-module.exports.pocketOauthAuthorize = (event, context, callback) => {
-  const params = JSON.parse(event.body);
-  const reqd = {
-    consumer_key: process.env.POCKET_CONSUMER_KEY,
-    code: params.code,
-  };
-  const data = JSON.stringify(reqd);
-  axios.post('https://getpocket.com/v3/oauth/authorize', data, HTTP_POST_CONFIG)
-    .then((res) => {
-      const bodyObj = {
-        access_token: res.data.access_token,
-        username: res.data.username,
-      };
-      callback(null, successResponseBuilder(bodyObj));
-    })
-    .catch((error) => {
-      debug(error);
-      callback(null, errorResponseBuilder(error));
-    });
-};
-
-module.exports.pocketGet = (event, context, callback) => {
-  const params = JSON.parse(event.body);
-  const reqd = {
-    consumer_key: process.env.POCKET_CONSUMER_KEY,
-    access_token: params.access_token,
-  };
-  Pocket.get(reqd.consumer_key, reqd.access_token, params)
-    .then((res) => {
-      callback(null, successResponseBuilder(res));
-    })
-    .catch((error) => {
-      debug(error);
-      callback(null, errorResponseBuilder(error));
-    });
-};
-
-module.exports.pocketSendArchive = (event, context, callback) => {
-  const params = event.queryStringParameters;
-  const q = new URLSearchParams();
-  q.append('consumer_key', process.env.POCKET_CONSUMER_KEY);
-  q.append('access_token', params.access_token);
-  q.append('actions', `[{"action":"archive","item_id":${params.item_id}}]`);
-  axios.get(`https://getpocket.com/v3/send?${q.toString()}`, HTTP_GET_CONFIG)
-    .then((res) => {
-      const bodyObj = res.data;
-      callback(null, successResponseBuilder(bodyObj));
-    })
-    .catch((error) => {
-      debug(error);
-      callback(null, errorResponseBuilder(error));
-    });
-};
-
-module.exports.pocketSendFavorite = (event, context, callback) => {
-  const params = event.queryStringParameters;
-  const q = new URLSearchParams();
-  q.append('consumer_key', process.env.POCKET_CONSUMER_KEY);
-  q.append('access_token', params.access_token);
-  q.append('actions', `[{"action":"favorite","item_id":${params.item_id}}]`);
-  axios.get(`https://getpocket.com/v3/send?${q.toString()}`, HTTP_GET_CONFIG)
-    .then((res) => {
-      const bodyObj = res.data;
-      callback(null, successResponseBuilder(bodyObj));
-    })
-    .catch((error) => {
-      debug(error);
-      callback(null, errorResponseBuilder(error));
-    });
-};
-
-module.exports.pocketSendUnfavorite = (event, context, callback) => {
-  const params = event.queryStringParameters;
-  const q = new URLSearchParams();
-  q.append('consumer_key', process.env.POCKET_CONSUMER_KEY);
-  q.append('access_token', params.access_token);
-  q.append('actions', `[{"action":"unfavorite","item_id":${params.item_id}}]`);
-  axios.get(`https://getpocket.com/v3/send?${q.toString()}`, HTTP_GET_CONFIG)
-    .then((res) => {
-      const bodyObj = res.data;
-      callback(null, successResponseBuilder(bodyObj));
-    })
-    .catch((error) => {
-      debug(error);
-      callback(null, errorResponseBuilder(error));
-    });
-};
-
-module.exports.pocketSendTagsAdd = (event, context, callback) => {
-  const params = event.queryStringParameters;
-  const q = new URLSearchParams();
-  q.append('consumer_key', process.env.POCKET_CONSUMER_KEY);
-  q.append('access_token', params.access_token);
-  q.append('actions', `[{"action":"tags_add","item_id":${params.item_id},"tags":"${params.tag}"}]`);
-  axios.get(`https://getpocket.com/v3/send?${q.toString()}`, HTTP_GET_CONFIG)
-    .then((res) => {
-      const bodyObj = res.data;
-      callback(null, successResponseBuilder(bodyObj));
-    })
-    .catch((error) => {
-      debug(error);
-      callback(null, errorResponseBuilder(error));
-    });
+  const data = JSON.stringify(Object.assign(reqd, params));
+  const endpoint = 'https://getpocket.com/v3/get';
+  return axios.post(endpoint, data, HTTP_POST_CONFIG)
+    .then(res => res.data);
 };
