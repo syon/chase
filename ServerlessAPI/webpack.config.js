b42da0fefd159ad50cc68d186eb95ff4f794edb0
syon <syon.xiii@gmail.com>
2017-12-31 13:44:39 +0900

    :package: file-loader

--- ServerlessAPI/webpack.config.js
+++ ServerlessAPI/webpack.config.js
@@ -18,6 +18,17 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+        ],
+      },
     ],
   },
 };
