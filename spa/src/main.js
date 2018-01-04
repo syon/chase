dc8d687910f46fdccd6441b532746335bdc9cb43
syon <syon.xiii@gmail.com>
2018-01-04 23:58:10 +0900

    :package: css-browser-selector

--- spa/src/main.js
+++ spa/src/main.js
@@ -6,6 +6,8 @@ import { sync } from 'vuex-router-sync';
 import VueAnalytics from 'vue-analytics';
 import 'babel-polyfill';
 import 'whatwg-fetch';
+import 'css-browser-selector';
+
 import App from './App';
 import router from './router';
 import store from './store';
